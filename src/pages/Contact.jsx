cfa50d774ca9a58dd249821ef593f8076fa4a515
M7UNDO <165899552+M7UNDO@users.noreply.github.com>
2026-03-25 01:53:38 +0200

    Test

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
+import MapSection from "../components/MapSection";
 import "../styles/Contact.css";
 
 export default function Contact() {
   return(
     <>
     <ContactForm/>
+    <MapSection/>
     </>
 
   );
